c307b8663365b33badaa2ffd4e73c8cae194c833
DouglasVulcano <vulcanodouglas1708@gmail.com>
2025-03-21 01:42:17 -0300

    implements db connection

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,25 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from 'src/user/user.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }), // Carrega as variáveis do .env
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      autoLoadEntities: process.env.DB_AUTOLOAD_ENTITIES === 'true',
+      synchronize: process.env.DB_SYNCHRONIZE === 'true',
+    }),
+    UserModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
